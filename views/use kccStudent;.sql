d58476968f2f7c2f7ee9c4c6dc36dd2849010871
saurabh kumar verma <saurbhv84btechcse2024@kccitm.edu.in>
2022-06-13 15:51:13 +0530

    Creating table in DB

---
+++ views/use kccStudent;.sql
@@ -0,0 +1,10 @@
+use kccStudent;
+select * from kccStudent;
+
+CREATE TABLE saurabh (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
