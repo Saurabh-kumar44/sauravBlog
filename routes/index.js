15ec7fbdcf00a8f1e0c5e4d77f469b9645f3c10d
saurabh kumar verma <saurabhv84btechcse2024@kccitm.edu.in>
2022-06-20 13:12:18 +0530

    all

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');//using underscore
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req);//print the req
-  res.json({name:"Saurav"}); 
+  // res.json({name:"Saurav"}); 
   // res.render('index', { title: 'Express' });
+
+  console.log("By Map");
+  console.log(_.map([1, 2, 3], function(num){ return num * 3; }));//using map function
+  res.json(_.map([1, 2, 3], function(num){ return num * 3; }))
+  console.log("by sort by");
+  console.log(_.sortBy([1, 2, 3, 4, 5, 6], function(num){ return Math.sin(num); }));
+
+  
  
 });
 
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 var mysql = require('mysql');
 // var modelLogin = require("../model/login") //calling another file or exporting from login
 // router.get('/', function(req, res) {
@@ -10,60 +11,84 @@ var mysql = require('mysql');
 
 //Making connection to mysql
 // setting up host,user,name to sql 
-con.connect()
+// con.connect();
 var con = mysql.createConnection({
- host: "192.168.17.44",
- user: "root",
- password: "1234"
+  host: "easylearning.guru",
+  user: "kcc_student",
+  password: "Kccitm.edu.in1",
+  database: " kccStudent"
 });
 
  
-router.get('/', function(req, res, next) {
-  console.log("saurabh");
-  con.connect(function(err) {
-      console.log("HARSH")
-      if (err) console.log("err");
-      else console.log("Connected!");
-    });
-  // modelLogin.a(modelLogin.pandy.name)
-console.log("kisna");
-console.log("SIR");
-console.log("Butti");
+router.get('/', function(req, res) {
   res.render('login');
+con.connect(function (err) {
+   if (err) console.log("err");
+   else console.log("Connected!");
+});
+  // modelLogin.a(modelLogin.pandy.name)
+// console.log("kisna");
+// console.log("SIR");
+// console.log("Butti");
+//   res.render('login');
 });
 
 //Inserting into mysql DB
-router.post('/getLogin', function(req, res, next) {
-  console.log(req.body)
-  // res.send({"name":"himani"})
-  //res.render('teacher')
-  console.log(req.body.pass)
-  var sql = "INSERT INTO `login` (`email`, `pass`) \
-  VALUES ('"+req.body.Ashu+"', '"+req.body.pass+"');"
+router.all('/insert', function(req, res) {
+  var sql = "INSERT INTO `SAURABHV` (`email`, `pass`) \
+  VALUES ('"+req.body.fname+"','"+req.body.lname+"');"
   console.log(sql)
- con.connect()
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result);
-
-    });
-res.json({"Name":req.body})
+  con.connect(
+     con.query(sql,function(err,result){
+        console.log(result);
+     })
+  );
+  
 });
+
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host : 'easylearning.guru',
+  user :  'kcc_student',
+  password : 'Kccitm.edu.in1',
+  database : 'kccStudent'
+});
+
+router.get('/read', function(req, res) {
+  pool.getConnection(function (err,connection){
+     connection.query("SELECT * FROM SAURABHV",function(err,rows){
+        connection.release();
+        if(err) throw err;
+
+        console.log(rows.length);
+        res.send(JSON.stringify(rows));
+     });
+  });
+ 
+});
+
+
+
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
-//   console.log(req);
-console.log("I am fine")
+// router.get('/', function(req, res) {
+// //   console.log(req);
+// console.log("I am fine")
+//   res.render('login');
+// });
+router.all('/upgateLogin', function(req, res) {
   res.render('login');
+  con.query("")
+ 
 });
 
 // // using by get
-router.get('/getLogin', function(req, res, next) {
-    console.log(req.query);
-    // res.render({"name":"himani"});
-    res.json({"name":req.query.Ashu});
-  });
+// router.get('/getLogin', function(req, res, next) {
+//     console.log(req.query);
+//     // res.render({"name":"himani"});
+//     res.json({"name":req.query.Ashu});
+//   });
 
   //by post
   // router.post('/getLogin', function(req, res, next) {
@@ -83,7 +108,7 @@ router.get('/getLogin', function(req, res, next) {
   //  router.post('/getLogin', function(req, res) {
   //   // console.log(req.query);
   //   // res.render({"name":"himani"});
-  //   res.json({"Email":req.body.email,"Pass":req.body.password});
+  //   res.json({"Email":req.body.email,"Pass":req.body.pass});
   // });
 
   //by all- used no matter sending by get or post it will hit here
@@ -99,4 +124,3 @@ router.get('/getLogin', function(req, res, next) {
     // res.json({"name":req.body.Ashu});
 //   });
 
-module.exports = router;
